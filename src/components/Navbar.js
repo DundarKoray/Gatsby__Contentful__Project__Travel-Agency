386c502fad62d4e2a8b0351192e22dec8a53760e
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-19 21:43:28 +0300

    completed navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'gatsby';
+import styles from '../css/navbar.module.css';
+import { FaAlignRight } from 'react-icons/fa';
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+import logo from '../images/logo.svg'
 
 const Navbar = () => {
+    const [isOpen, setNav] = useState(false)
+
+    const toggleNav = () => {
+        setNav(isOpen => !isOpen) 
+    }
+
+    console.log(isOpen)
+    
     return (
-        <div>
-            <h1> I'm navbar</h1>
-        </div>
+       <nav className={styles.navbar}>
+           <div className={styles.navCenter}>
+               <div className={styles.navHeader}>
+                   <img src={logo} alt="company-logo"/>
+                   <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+                       <FaAlignRight className={styles.logoIcon} />
+                   </button>
+               </div>
+               <ul className={isOpen ? `${styles.navLinks} ${styles.showNav}` : `${styles.navLinks}`}>
+                   {links.map((item, index) =>{
+                       return (
+                       <li key={index}> <Link to={item.path}>{item.text}</Link></li>
+                        )
+                   })}
+               </ul>
+               <div className={styles.navSocialLinks}>
+                   {socialIcons.map((item,index)=>{
+                       return (
+                            <a 
+                                key={index} 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                href={item.url}
+                            >
+                                {item.icon}
+                            </a>
+                        )
+                   })}
+               </div>
+           </div>
+       </nav>
     );
 };
 
