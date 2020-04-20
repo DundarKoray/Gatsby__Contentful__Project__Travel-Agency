d172b18105e09a47153665152180f2a1d9ff390b
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-20 15:50:13 +0300

    completed error 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Layout from '../components/Layout'
+import { Link } from 'gatsby'
+import Banner from '../components/Banner/Banner'
 
 const error = () => {
+    const style = {
+        background: "#3fd0d4",
+        minHeight: "calc(100vh - 62px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
     return (
         <Layout>
-            hello from error
+            <header style={style}>
+                <Banner title="Oops it's a dead end">
+                    <Link className="btn-white" to="/">Back to home page</Link>
+                </Banner>
+            </header>
         </Layout>
     );
 };
