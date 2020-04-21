a10994336fdd5a10f2f423e3229fc58f0e97aaa8
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-21 17:06:18 +0300

    wrote alternative syntax for TitleStyled component

--- src/components/TitleStyled/TitleStyled.js
+++ src/components/TitleStyled/TitleStyled.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const TitleStyled = ({ title, subtitle }) => {
     return (
         <TitleWrapper>
@@ -40,3 +41,40 @@ const TitleWrapper = styled.div `
 `
 
 export default TitleStyled;
+
+
+
+/*
+// alternative
+
+const TitleStyled = ({ title, subtitle, className }) => {
+    return (
+        <div className={className}>
+            <h4>
+                <span className="title">{title}</span>
+                <span>{subtitle}</span>
+            </h4>
+        </div>
+    );
+};
+
+export default styled(TitleStyled) `
+    text-transform: uppercase;
+    font-size: 2.3rem;
+    margin-bottom: 2rem;
+    
+    h4 {
+        text-align: center;
+        letter-spacing: 7px;
+        color: var(--primaryColor);
+    }
+    
+    .title {
+        color: var(--mainBlack);
+    }
+    
+    span {
+        display: block;
+    }
+
+*/
