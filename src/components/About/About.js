be018d2265a76ebeb73e21dbf1f3feb84bdef18c
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-21 20:38:53 +0300

    completed about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import TitleStyled from '../TitleStyled/TitleStyled'
+import styles from './about.module.css'
+import img from './images/defaultBcg.jpeg'
 
 const About = () => {
     return (
-        <div>
+        <section className={styles.about}>
             <TitleStyled title="about" subtitle="us"/>
-        </div>
+            <div className={styles.center}>
+                <article className={styles.img}>
+                    <div className={styles.imgContainer}>
+                        <img src={img} alt="about company"/>
+                    </div>
+                </article>
+                <article className={styles.info}>
+                    <h4>explore the difference</h4>
+                    <p>Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis</p>
+                    <p>Habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas</p>
+                    <button type="button" className="btn-primary">red more</button>
+                </article>
+            </div>
+        </section>
     );
 };
 
