6074eefa8fddb91464a381ae44bde095aabd9fe4
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-22 10:59:37 +0300

    added siteMetadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
+    siteMetadata: {
+        title: "TravelAgency",
+        description: "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+        author: "@koraydundar"
+    },
     plugins: [`gatsby-plugin-styled-components`]
 }
+
