3731e77f2672de5948fc3bec3b57d18842e001f2
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-22 15:26:08 +0300

    tested page query

--- src/pages/Tours.js
+++ src/pages/Tours.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout'
-import Header from '../examples/Header'
-import RegularHeader from '../examples/RegularHeader'
+
 
 class tours extends Component {
     render() {
         return (
             <Layout>
-                <Header />
-                <RegularHeader />
+                
             </Layout>
         );
     }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const blog = () => {
-    return (
-        <Layout>
-            hello from blog page
-        </Layout>
-    );
-};
+const blog = ({ data }) => {
+    // console.log(props)
+    // this data comes from props, console.log(props) to see
 
-export default blog;
+  return (
+    <Layout>
+      hello from blog page
+      <h1>title: {data.site.siteMetadata.title}</h1>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
+
+export default blog
