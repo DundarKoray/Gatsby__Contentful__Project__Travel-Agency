41dc5281e9e989f9ab2e867b7509b78099e1e20d
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-22 11:20:18 +0300

    added alternative way of writing query

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -25,6 +25,9 @@ const Header = () => {
 
 export default Header
 
+
+
+
 // destructring
 // const Header = () => {
 //     const {site} = useStaticQuery(graphql`
@@ -50,6 +53,10 @@ export default Header
   
 //   export default Header
 
+
+
+
+
 // destructring further
 // const Header = () => {
 //         const {site:{siteMetadata}} = useStaticQuery(graphql`
@@ -74,3 +81,31 @@ export default Header
 //       }
       
 //       export default Header
+
+
+
+
+// this way of writing query is exactly same as the top one
+// const Header = () => {
+//     const data = useStaticQuery(graphql`
+//       query Koray{
+//         site {
+//           siteMetadata {
+//             title
+//             description
+//             author
+//           }
+//         }
+//       }
+//     `)
+  
+//     console.log(data)
+//     return (
+//       <div>
+//         <h1>title: {data.site.siteMetadata.title}</h1>
+//         <h1>author: {data.site.siteMetadata.author}</h1>
+//       </div>
+//     )
+//   }
+  
+//   export default Header
