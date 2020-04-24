7586485958f76d4a62ab1dd44b7195dff49408fc
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-24 15:51:44 +0300

    added labels for the document

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import TitleStyled from '../TitleStyled/TitleStyled'
-import styles from './contact.module.css'
+import React from "react"
+import TitleStyled from "../TitleStyled/TitleStyled"
+import styles from "./contact.module.css"
 
 const Contact = () => {
-    return (
-        <section className={styles.contact}>
-            <TitleStyled title="contact" subtitle="us" />
-            <div className={styles.center}>
-                <form className={styles.form}>
+  return (
+    <section className={styles.contact}>
+      <TitleStyled title="contact" subtitle="us" />
+      <div className={styles.center}>
+        <form className={styles.form}>
+          {/* name */}
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className={styles.formControl}
+              placeholder="name"
+            />
+          </div>
 
-                    {/* name */}
-                    <div>
-                        <input 
-                          type="text" 
-                          name="name" 
-                          id="name"
-                          className={styles.formControl}
-                          placeholder="name"
-                        />
-                    </div>
+          {/* email */}
+          <div>
+            <label htmlFor="email">Email</label>
 
-                    {/* email */}
-                    <div>
-                        <input 
-                          type="email" 
-                          name="email" 
-                          id="email"
-                          className={styles.formControl}
-                          placeholder="email"
-                        />
-                    </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className={styles.formControl}
+              placeholder="email"
+            />
+          </div>
 
-                    {/* message */}
-                    <div>
-                        <textarea 
-                          name="message" 
-                          id="message"
-                          className={styles.formControl}
-                          rows="10"
-                          placeholder="message"
-                        />
-                    </div>
-                    
-                    {/* submit */}
-                    <div>
-                        <input 
-                          type="submit" 
-                          value="send"
-                          className={styles.submit}
-                        />
-                    </div>
-                </form>
-            </div>
-        </section>
-    );
-};
+          {/* message */}
+          <div>
+            <label htmlFor="message">Message</label>
 
-export default Contact;
+            <textarea
+              name="message"
+              id="message"
+              className={styles.formControl}
+              rows="10"
+              placeholder="message"
+            />
+          </div>
+
+          {/* submit */}
+          <div>
+            <input type="submit" value="send" className={styles.submit} />
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
