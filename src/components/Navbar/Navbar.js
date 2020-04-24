b27ffd245d8a134f52bff5a9e4a5ab99165f7f56
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-24 15:59:16 +0300

    removed the old link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-// import { Link } from 'gatsby';
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import styles from "./navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
@@ -35,7 +34,7 @@ const Navbar = () => {
           {links.map((item, index) => {
             return (
               <li key={index}>
-                {/* <Link to={item.path}>{item.text}</Link> */}
+                
                 <AniLink fade to={item.path}>{item.text}</AniLink>
               </li>
             )
