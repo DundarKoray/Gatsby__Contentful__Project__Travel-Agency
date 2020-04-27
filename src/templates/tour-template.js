e3edf0f31b42c0a33ee93f73842bad83f28d41df
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-27 10:17:28 +0300

    created tour template

---
+++ src/templates/tour-template.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Template = () => {
+    return (
+        <div>
+            hello from dumm data template
+        </div>
+    );
+};
+
+export default Template;
