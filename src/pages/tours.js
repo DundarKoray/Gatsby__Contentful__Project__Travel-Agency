643f258a71074eb55580d21c520ecd7a2a679e43
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-28 10:55:45 +0300

    recreated tours page to fix a bug

---
+++ src/pages/tours.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import HeroStyled from "../components/HeroStyled/HeroStyled"
+import Tours from "../components/Tours/Tours"
+
+const tours = (props) => {
+  return (
+    <Layout>
+      <HeroStyled
+        img={props.data.backgroundImage.childImageSharp.fluid}
+        fullSize="false"
+      />
+      <Tours />
+    </Layout>
+  )
+}
+
+export default tours
+
+export const query = graphql`
+  query {
+    backgroundImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
