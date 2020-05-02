31b7477895e3962b6228f02f1e14729a0234f318
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-02 12:36:26 +0300

    added query

--- src/templates/blog-pagination-template.js
+++ src/templates/blog-pagination-template.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import styles from './blog-template.css';
-import BlogCard from '../components/Blog/BlogCard';
-import TitleStyled from '../components/TitleStyled'
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import styles from "./blog-template.css"
+import BlogCard from "../components/Blog/BlogCard"
+import TitleStyled from "../components/TitleStyled"
 
-const BlogPaginationTemplate = () => {
-    return (
-        <Layout>this is a BlogPagination template</Layout>
-    );
-};
+const BlogPaginationTemplate = props => {
+  // console.log(props.data)
+  const { data } = props
 
-export default BlogPaginationTemplate;
+  return (
+    <section className={styles.blog}>
+        <TitleStyled title="latest" subtitle="posts" />
+        <div className={styles.center}>
+            {data.posts.edges.map(({node}) =>{
+                return <BlogCard key={node.id} blog={node} />
+            })}
+        </div>
+    </section>
+  ) 
+}
+
+export default BlogPaginationTemplate
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
