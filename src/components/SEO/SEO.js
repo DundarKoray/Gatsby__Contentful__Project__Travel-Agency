fffd707c27ac2a150fb2332ad884b8745e0631f7
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-03 12:08:45 +0300

    facebook site meta card added

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -29,6 +29,15 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+      {/* facebook cards */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      
 
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
