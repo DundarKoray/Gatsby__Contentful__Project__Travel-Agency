83180d5a53b257329b4d2176237255b5987796f0
Koray Dundar <koraydundar_40@hotmail.com>
2020-05-03 12:21:05 +0300

    installed sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     `gatsby-transformer-sharp`, 
     `gatsby-plugin-sharp`,
     `gatsby-plugin-transition-link`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
   ],
 }
